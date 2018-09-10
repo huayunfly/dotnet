f6433584b6d7f001a8eab763e35ccfa3050aa9d7
Yun Hua <huayunfly@126.com>
2018-09-10 13:28:21 +0800

    add dbconnection sample and test

---
+++ com.huayunfly.app/com.huayunfly.app.xunit.tests/DBConnectionSamplesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace com.huayunfly.app.xunit.tests
+{
+    public class DBConnectionSamplesTest
+    {
+        [Fact]
+        public void OpenConnectionTest()
+        {
+            Assert.True(DBConnectionSamples.OpenConnection());
+        }
+
+        [Fact]
+        public void OpenConnectionUsingConfigTest()
+        {
+            Assert.True(DBConnectionSamples.OpenConnectionUsingConfig());
+        }
+    }
+}
---
+++ com.huayunfly.app/com.huayunfly.app/DBConnectionSamples.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
+
+namespace com.huayunfly.app
+{
+    public class DBConnectionSamples
+    {
+        public static bool OpenConnection()
+        {
+            string connectionString = @"server=(localdb)\MSSQLLocalDB;" +
+                "integrated security=SSPI; database=Books";
+            return OpenConnection(connectionString);
+        }
+
+        public static bool OpenConnectionUsingConfig()
+        {
+            /* SetBasePath() is in Microsoft.Extensions.Configuration.Json */
+            var configurationBuilder = 
+                new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("db_connection_config.json");
+            IConfiguration config = configurationBuilder.Build();
+            string connectionString = config["Data:DefaultConnection:ConnectionString"];
+            return OpenConnection(connectionString);
+
+        }
+
+        private static bool OpenConnection(string connectionString)
+        {
+            bool success = false;
+            var connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                success = true;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"Database connection failed: {e.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return success;
+        }
+
+    }
+}
