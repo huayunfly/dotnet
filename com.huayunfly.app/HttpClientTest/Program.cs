43f1b844a67aa304d6532c7137c2cfd5b2d6abf8
huayunfly <huayunfly@126.com>
2018-12-01 17:10:56 +0800

    Add HttpClientTest

---
+++ com.huayunfly.app/HttpClientTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public static void Log(string prefix) => 
+            Console.WriteLine($"{prefix}, task: {Task.CurrentId}, " + 
+                $"thread: {Thread.CurrentThread.ManagedThreadId}, time: {DateTime.Now}");
+
+        static void Main(string[] args)
+        {
+            int NUMBER_OF_REQUESTS = 100;
+            ServicePointManager.DefaultConnectionLimit = 100;
+
+            Parallel.For(0, NUMBER_OF_REQUESTS, async i =>
+            {
+                HttpResponseMessage httpResponse = null;
+                Log($"S {i}");
+                httpResponse = await _httpClient.GetAsync("http://www.bing.com");
+                Log($"E {i}");
+            }
+            );
+            Console.ReadLine();
+        }
+    }
+}
